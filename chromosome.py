93285414392cea34bc30403084bf74df02a4f81c
SergIOSanchez99 <sergio.sanchez3@unmsm.edu.pe>
2024-11-06 03:23:59 -0500

    Creacion de la clase Chromosome

---
+++ chromosome.py
@@ -0,0 +1,15 @@
+import random
+
+# Clase Chromosome: genera individuos y población inicial
+class Chromosome:
+    def __init__(self, n_individuals=4):
+        self.n_individuals = n_individuals
+        self.population = self.create_population()
+
+    def create_individual(self):
+        # Genera un cromosoma con 6 alelos, cada uno con un valor entre 1 y 4
+        return {"chromosome": [random.randint(1, 4) for _ in range(6)], "fitness_total": 0}
+
+    def create_population(self):
+        # Crea una población con el número de individuos especificado
+        return [self.create_individual() for _ in range(self.n_individuals)]
