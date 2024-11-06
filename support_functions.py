ab1c2cdff979a6742b339b3b3b7168502959cd0a
ALEX241342 <axel.felix@unmsm.edu.pe>
2024-11-06 02:31:21 -0500

    test de union de clase robot con las funciones de soporte

--- support_functions.py
+++ support_functions.py
@@ -1,6 +1,20 @@
 # FUNCIONES DE SOPORTE O APOYO EN LA CONFIGURACION DEL LABERINTO Y/O ALGORITMO
 
 # funcion para obtener el valor maximo de genes y cromosomas posibles a generar por la función random
-def max_number_gen_cromosome (x_i,y_i,x_u,y_u):
-    max_number = abs(x_u-x_i) + abs(y_u-y_i)
+def max_number_gen_cromosome (actual_position,final_position):
+    max_number = abs(final_position[0]-actual_position[0]) + abs(final_position[1]-actual_position[1])
     return max_number
+
+def selection_move(option):
+    name_move=""
+    if(option==1):
+        name_move="up"
+    elif(option==2):
+        name_move="down"
+    elif(option==3):
+        name_move="left"
+    elif(option==4):
+        name_move="right"
+    return name_move
+
+    
